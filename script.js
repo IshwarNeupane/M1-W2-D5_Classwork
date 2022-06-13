bc61d91b799040ab4640da36b036c4c064fa9b0e
IshwarNeupane <ishwar1981@gmail.com>
2022-06-13 22:22:48 +0545

    This is my week2 day5 classwork and is continued from day4

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ function experience() {
     var drp = document.querySelector('.about-drop-content');
     drp.classList.toggle ('show');
 }
+function everything() {
+    var content = document.querySelector('.for-everything').classList.value;
+    alert(content);
+    if
+}
+function arts() {
+    
+}
+function creative() {
+    
+}
+function design() {
+    
+}
